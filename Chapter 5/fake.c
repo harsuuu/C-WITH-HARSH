9eac1fc96600e17645eea82e52f2fa6ba40e232e
harsuuu <harshjoshi7895@gmail.com>
2022-07-01 11:16:41 +0530

    my files

---
+++ Chapter 5/fake.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void display();
+int main(){
+    int a;
+    printf("harsh");
+    display();
+    printf("harshuuu");
+    return 0;
+}
+void  display(){
+printf("harshu");
+
+}
